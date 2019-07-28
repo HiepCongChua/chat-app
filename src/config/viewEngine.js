b9648325ef7c04a643928460fd096ac91f252c51
hieplm <leminhhiep@hoasao.vn>
2019-07-28 09:42:18 +0700

    [1.0.0] Add static pages

---
+++ src/config/viewEngine.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import expressEjsExtend from 'express-ejs-extend';
+const configViewEngine  = (app)=>{
+    app.use(express.static('./src/public'));
+    app.engine('ejs',expressEjsExtend);
+    app.set("view engine","ejs");
+    app.set("views","./src/views");
+};
+export default configViewEngine;
