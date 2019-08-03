67a856a52008948c79a719398ffbbc8bcc2fed1f
HiepCongChuaa <hieplevuc@gmail.com>
2019-08-04 01:47:56 +0700

    [1.0.0] deploy_cluster

--- src/config/session.js
+++ src/config/session.js
@@ -1,20 +1,25 @@
 import session from 'express-session';
 import connectMongo from 'connect-mongodb-session';
 const MongoStore = connectMongo(session);
-const URI = `${process.env.DB_CONNECTION}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+// const URI = `${process.env.DB_CONNECTION}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+// const store = new MongoStore({
+//   uri : URI,
+//   collection : 'sessions'
+// });
+const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PW}@cluster0-kv3pr.mongodb.net/${process.env.DATABASE_NAME}`;
 const store = new MongoStore({
-  uri : URI,
+  uri : MONGODB_URI,
   collection : 'sessions'
 });
 const configSession=(app)=>
 {
 app.use(session({
     secret:'my secret',
-    resave:true,
+    resave:false,
     saveUninitialized:false,
     store : store,
     name : 'chat-app',
     cookie:{maxAge:1000*60*60*24}
-}))
+}));
 }
 export default configSession;
