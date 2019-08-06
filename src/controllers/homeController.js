73dd63ab7cd73ce95eec5036fd44438b73d5f92b
HiepCongChuaa <hieplevuc@gmail.com>
2019-08-06 23:13:26 +0700

    [1.0.0] render_notification

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,11 @@
-const getHome = (req, res, next) => {
+import {getNotifications} from '../services/notificationService';
+const getHome = async (req, res, next) => {
+    let notifications = await getNotifications(req.user._id);
     return res.render('main/home/home',{
         errors: req.flash("errors"),
         success: req.flash("success"),
-        user:req.user
+        user:req.user,
+        notifications
 
     })
 };
